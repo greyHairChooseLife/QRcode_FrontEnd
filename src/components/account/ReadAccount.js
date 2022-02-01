5c8f2dcb096118f373325e16850e5962f485b2cd
sy <greyhairchooselife@gmail.com>
2022-02-01 23:41:58 +0900

    update버튼 생성, 버튼 클릭하면 input태그로 변하는 것 처럼 보이도록 ReadAccount 컴포넌트와 UpdateComponent로 구분하고 this.state.mode를 이용해 전환

--- src/components/account/ReadAccount.js
+++ src/components/account/ReadAccount.js
@@ -5,13 +5,18 @@ class ReadAccount extends Component{
 		let list = [];
 		const data = this.props.data;
 		for(var i=0; i<data.length; i++){
+			(function(m){
 			list.push(
-				<div key={data[i].id}>
-					<span>{data[i].id} </span>
-					<span>{data[i].name} </span>
-					<span>{data[i].registered_date}</span>
+				<div key={data[m].id}>
+					<span>{data[m].id}</span>
+					<span>{data[m].name}</span>
+					<span>{data[m].registered_date}</span>
+					<button onClick={function(){
+						this.props.onUpdate('update_account', m);
+					}.bind(this)}>update</button>
 				</div>
 			)
+			}.bind(this))(i)
 		}
 		return (
 			<div className="ReadAccount">
--- src/components/account/UpdateAccount.js
+++ src/components/account/UpdateAccount.js
@@ -3,16 +3,38 @@ import React, { Component } from 'react';
 class UpdateAccount extends Component{
 	render(){
 		let list = [];
-		const data = this.props.data_length;
-		for(var i=0; i<data; i++){
+		const data = this.props.data;
+		for(var i=0; i<data.length; i++){
+			if(i === this.props.target)
+				list.push(
+					<div key={data[i].id}>
+					<form onSubmit={function(e){
+						e.preventDefault();
+
+					}.bind(this)}>
+						<span>{data[i].id}</span>
+						<input type="text" name="name" defaultValue={data[i].name} />
+						<span>{data[i].registered_date}</span>
+						<button>apply</button>
+						<button>cancel</button>
+					</form>
+					</div>
+
+				)
+			else{
 			list.push(
-				<div key={i}>
+				<div key={data[i].id}>
+					<span>{data[i].id}</span>
+					<span>{data[i].name}</span>
+					<span>{data[i].registered_date}</span>
 					<button>update</button>
 				</div>
 			)
+			}
 		}
 		return (
 			<div className="UpdateAccount">
+				<p>update procedure</p>
 				{list}
 			</div>
 		);
