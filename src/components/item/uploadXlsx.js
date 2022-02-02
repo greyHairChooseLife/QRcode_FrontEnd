65818bd291efa46842217d3800f47a1fde886a11
sy <greyhairchooselife@gmail.com>
2022-02-02 21:54:52 +0900

    Control_item 컴포는트 추가. uploadXlsx 컴포넌트 추가 : 엑셀 파일 읽고 state에 객체로 저장하는 기능까지

--- src/components/item/uploadXlsx.js
+++ src/components/item/uploadXlsx.js
@@ -7,30 +7,55 @@ const api = axios.create({
 })
 
 class ControlItem extends Component{
+	constructor() {
+		super();
+		this.state = {
+			list: null,
+		}
+	}
 	getItemToControl = async (account_id) => {
 		const result = await api.get(`/items/control_item?account_id=${account_id}`);
 		this.props.resetItem(result.data);
 	}
+	readFile = async (e) => {
+		const file = e.target.uploaded.files[0];
+		const data = await file.arrayBuffer();
+		const workbook = xlsx.read(data);
+		const first_sheet_name = workbook.SheetNames[0];
+		const jsonData = xlsx.utils.sheet_to_json(workbook.Sheets[first_sheet_name]);
+		this.setState({
+			list: jsonData,
+		});
+	}
 
 	render(){
-		let list = [];
+		let uploadFunction = 
+			<form onSubmit={function(e){
+				e.preventDefault();
+				this.readFile(e);
+			}.bind(this)}>
+				<input type="file" accept=".xlsx" name="uploaded" />
+				<br />
+				<input type="submit" value="확인" readOnly />
+			</form>;
+		let items = [];
+		if(this.state.list !== null){
+			for(var i=0; i<this.state.list.length; i++){
+				items.push(
+					<div key={i}>
+						<span>{this.state.list[i]['상품코드']}</span>
+						<span>{this.state.list[i]['상품명']}</span>
+						<span>{this.state.list[i]['매입원가']}</span>
+						<span>{this.state.list[i]['규격']}</span>
+					</div>
+				);
+			}
+		}
 
 		return (
 			<div className="uploadXlsx">
-				<form onSubmit={async function(e){
-					e.preventDefault();
-					const file = e.target.uploaded.files[0];
-					const data = await file.arrayBuffer();
-					const workbook = xlsx.read(data);
-
-					let first_sheet_name = workbook.SheetNames[0];
-					let sheet = workbook.Sheets[first_sheet_name];
-					console.log(sheet);
-				}.bind(this)}>
-					<input type="file" accept=".xlsx" name="uploaded" />
-					<br />
-					<input type="submit" value="확인" readOnly />
-				</form>
+				{uploadFunction}
+				{items}
 			</div>
 		);
 	}
